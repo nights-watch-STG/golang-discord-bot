63c4e6682fbb0ccbd10dc25d2d1e5a02f7e45caf
Sandhu-Sahil <sandhu.sahil2002@gmail.com>
2023-04-30 23:53:58 +0530

    just changed the file

--- handler.go
+++ handler.go
@@ -1,12 +1,80 @@
 package main
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"fmt"
+	"strings"
 
-func HandleBot() {
-	Bot.AddHandler(messageHandler)
-	Bot.Identify.Intents = discordgo.IntentsGuildMessages
-	err := Bot.Open()
-	if err != nil {
+	"github.com/bwmarrin/discordgo"
+)
+
+func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore all messages created by the bot itself
+	if m.Author.ID == BotID {
 		return
+	}
+
+	// message contain some text and the bot mention
+	switch {
+	case strings.Contains(strings.ToUpper(m.Content), "BOT"):
+		{
+			_, err = s.ChannelMessageSend(m.ChannelID, "Hi there!")
+			if err != nil {
+				fmt.Print(err)
+			}
+		}
+	case strings.Contains(strings.ToUpper(m.Content), "HELLO"):
+		{
+			_, err = s.ChannelMessageSend(m.ChannelID, "Hello sir, how can I help you!")
+			if err != nil {
+				fmt.Print(err)
+			}
+		}
+	case strings.Contains(strings.ToUpper(m.Content), "HI"):
+		{
+			_, err = s.ChannelMessageSend(m.ChannelID, "Hello sir, how can I help you!")
+			if err != nil {
+				fmt.Print(err)
+			}
+		}
+	case strings.Contains(strings.ToUpper(m.Content), "LTV"):
+		{
+			_, err = s.ChannelMessageSend(m.ChannelID, "tu LTV panchooda")
+			if err != nil {
+				fmt.Print(err)
+			}
+		}
+
+	}
+
+	// if message contain personal mentions
+	if m.ContentWithMentionsReplaced() == `<@&998929334645563502>` { //@fudi deya
+		_, err = s.ChannelMessageSend(m.ChannelID, "kithe marre aa kuriyave sare")
+		if err != nil {
+			fmt.Print(err)
+		}
+	}
+	if m.ContentWithMentionsReplaced() == `<@&765554608172957696>` { //@kings
+		_, err = s.ChannelMessageSend(m.ChannelID, "Acha, gusse sare king bnate aa")
+		if err != nil {
+			fmt.Print(err)
+		}
+	}
+	if m.ContentWithMentionsReplaced() == `<@&789483612072181771>` { //@ace
+		_, err = s.ChannelMessageSend(m.ChannelID, "aces te oda he FUDU aa")
+		if err != nil {
+			fmt.Print(err)
+		}
+	}
+	if m.ContentWithMentionsReplaced() == `<@&1090312377876107324>` { //@shady sandhu sunyara squad
+		_, err = s.ChannelMessageSend(m.ChannelID, "serious kaamm aa jalde kathe hovo")
+		if err != nil {
+			fmt.Print(err)
+		}
+	}
+	if m.ContentWithMentionsReplaced() == `@Sahil Sandhu` { // personal
+		_, err = s.ChannelMessageSend(m.ChannelID, "peo nu kato vaja marre janda")
+		if err != nil {
+			fmt.Print(err)
+		}
 	}
 }
--- helper.go
+++ helper.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"os"
-	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -12,29 +11,11 @@ func NewBot() (*discordgo.Session, error) {
 	return goBot, err
 }
 
-func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// Ignore all messages created by the bot itself
-	if m.Author.ID == BotID {
+func HandleBot() {
+	Bot.AddHandler(messageHandler)
+	Bot.Identify.Intents = discordgo.IntentsGuildMessages
+	err := Bot.Open()
+	if err != nil {
 		return
-	}
-
-	if strings.ToUpper(m.Content) == "LTV" {
-		_, _ = s.ChannelMessageSend(m.ChannelID, "tu LTV panchooda")
-	}
-	if m.ContentWithMentionsReplaced() == `<@&998929334645563502>` { //@fudi deya
-		_, _ = s.ChannelMessageSend(m.ChannelID, "kithe marre aa kuriyave sare")
-	}
-	if m.ContentWithMentionsReplaced() == `<@&765554608172957696>` { //@kings
-		_, _ = s.ChannelMessageSend(m.ChannelID, "Acha, gusse sare king bnate aa")
-	}
-	if m.ContentWithMentionsReplaced() == `<@&789483612072181771>` { //@ace
-		_, _ = s.ChannelMessageSend(m.ChannelID, "aces te oda he FUDU aa")
-	}
-	if m.ContentWithMentionsReplaced() == `<@&1090312377876107324>` { //@shady sandhu sunyara squad
-		_, _ = s.ChannelMessageSend(m.ChannelID, "serious kaamm aa jalde kathe hovo")
-	}
-
-	if m.ContentWithMentionsReplaced() == `@Sahil Sandhu` { // personal
-		_, _ = s.ChannelMessageSend(m.ChannelID, "peo nu kato vaja marre janda")
 	}
 }
